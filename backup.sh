e499bd226b0baed1de0d63d1dfec7478344d8a9f
dipper <anonimous632@gmail.com>
2020-02-18 11:54:21 -0400

    script para realizar backup

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# cria um diretorio vazio em um dispositivo e copia os arquivos
+# do diretorio home para la
+#
+# adiciona o path do dispositivo em uma variavel e cria um
+# diretorio no dispositivo
+
+path=$1
+mkdir -v  $path/backup
+
+#copia os arquivos e diretorios do home do usuario
+cp -riv ~/* $path/backup/
